ebd894734abf614667f905ae5a7b6d213225bdcd
Salties <y.yan@bristol.ac.uk>
2015-02-11 18:35:32 +0000

    Added SingleOrEven

--- tinydtls-0.8.1/mi_util.h
+++ tinydtls-0.8.1/mi_util.h
@@ -1,6 +1,10 @@
 #ifndef MI_UTIL_H
 #define MI_UTIL_H
 
+#include <stdlib.h>
+
 unsigned int mi_rand();
+void SingleOrEven_C(void *buf, size_t *len);
+int SingleOrEven_S(void *inbuf, size_t inlen, void* outbuf, size_t *outlen);
 
 #endif
