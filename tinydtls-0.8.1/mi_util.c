a3ab8be66b7df14ed1c4665d1fbb9741a1b93975
Salties <y.yan@bristol.ac.uk>
2015-02-24 13:33:30 +0000

    Update

--- tinydtls-0.8.1/mi_util.c
+++ tinydtls-0.8.1/mi_util.c
@@ -14,64 +14,61 @@
 #define MAX_PACKET 1400
 
 //For general use.
-unsigned int
-mi_rand ()
+unsigned int mi_rand()
 {
-  static int fd = 0;
-  unsigned int r;
+    static int fd = 0;
+    unsigned int r;
 
-  if (!fd)
-    {
-      if (-1 == (fd = open ("/dev/urandom", O_RDONLY)))
-	{
-	  perror ("open");
-	  fd = 0;
-	  return 0;
-	}
-    }
-  if (-1 == (read (fd, &r, sizeof (r))))
-    perror ("read");
-  return r;
+    if (!fd)
+      {
+	  if (-1 == (fd = open("/dev/urandom", O_RDONLY)))
+	    {
+		perror("open");
+		fd = 0;
+		return 0;
+	    }
+      }
+    if (-1 == (read(fd, &r, sizeof(r))))
+	perror("read");
+    return r;
 }
 
 
 //For Single Or Even toy
-void
-SingleOrEven_C (void *buf, size_t * len)
+void SingleOrEven_C(void *buf, size_t * len)
 {
-  unsigned int r;
+    unsigned int r;
 
-  *len = sizeof (r);
-  r = mi_rand ();
-  memcpy (buf, &r, *len);
+    *len = sizeof(r);
+    r = mi_rand();
+    memcpy(buf, &r, *len);
 
-  return;
+    return;
 }
 
-int
-SingleOrEven_S (void *inbuf, size_t inlen, void *outbuf, size_t * outlen)
+int SingleOrEven_S(void *inbuf, size_t inlen, void *outbuf, size_t * outlen)
 {
-  unsigned int *r = inbuf;
-  printf ("Number:\t%u\n", *r);
+    unsigned int *r = inbuf;
+    printf("Number:\t%u\n", *r);
 
-  if (*r % 2)
-    {
-      printf ("Single\n");
-      strncpy (outbuf, "Single\n", *outlen);
-    }
-  else
-    {
-      printf ("Even\n");
-      strncpy (outbuf, "Even\n", *outlen);
-    }
-  *outlen = strlen (outbuf);
+    if (*r % 2)
+      {
+	  printf("Single\n");
+	  strncpy(outbuf, "Single\n", *outlen);
+      }
+    else
+      {
+	  printf("Even\n");
+	  strncpy(outbuf, "Even\n", *outlen);
+      }
+    *outlen = strlen(outbuf);
 
-  return HAS_RESPOND;
+    return HAS_RESPOND;
 }
 
 
 //For Leaky Coffee toy
-typedef enum {AMERICANO, CAPPUCCINO, ESPRESSO, MOCHA, FLAVOUR, EMPTY} Coffee;
+typedef enum { AMERICANO, CAPPUCCINO, ESPRESSO, MOCHA, FLAVOUR, EMPTY } Coffee;
 #define NCOFFEE 4
 #define STR_AMERICANO "AMERICANO"
 #define STR_CAPPUCCINO "CAPPUCCINO"
@@ -83,125 +80,133 @@ typedef enum {AMERICANO, CAPPUCCINO, ESPRESSO, MOCHA, FLAVOUR, EMPTY} Coffee;
 #define SUGAR "*"
 #define MILK "@"
 
-int
-OrderCoffee_C (uint8_t * buf, size_t * len)
+int OrderCoffee_C(uint8_t * buf, size_t * len)
 {
-  Coffee choice = mi_rand() % NCOFFEE;
-  char * coffeename;
+    Coffee choice = mi_rand() % NCOFFEE;
+    char *coffeename;
 
-  switch(choice)
-  {
-	  case AMERICANO:
-		  coffeename = STR_AMERICANO;
-		  break;
+    switch (choice)
+      {
+      case AMERICANO:
+	  coffeename = STR_AMERICANO;
+	  break;
 
-	  case CAPPUCCINO:
-		  coffeename = STR_CAPPUCCINO;
-		  break;
-		  
-	  case ESPRESSO:
-		  coffeename = STR_ESPRESSO;
-		  break;
+      case CAPPUCCINO:
+	  coffeename = STR_CAPPUCCINO;
+	  break;
 
-	  case MOCHA:
-		  coffeename = STR_MOCHA;
-		  break;
+      case ESPRESSO:
+	  coffeename = STR_ESPRESSO;
+	  break;
 
-	  default:
-		  coffeename = "OUT_OF_ORDER";
-		  break;
-  }
+      case MOCHA:
+	  coffeename = STR_MOCHA;
+	  break;
 
-  strcpy((char*)buf, coffeename);
-  *len = strlen(coffeename) + 1; // +1 for '\0'.
+      default:
+	  coffeename = "OUT_OF_ORDER";
+	  return 0;
+	  break;
+      }
 
-  return 0;
+    strcpy((char *) buf, coffeename);
+    *len = strlen(coffeename) + 1;	// +1 for '\0'.
+
+    return 1;
 }
 
-int
-LeakyCoffee_C (uint8_t * buf, size_t * len)
+int LeakyCoffee_C(uint8_t * buf, size_t * len)
 {
-  return 0;
+    return 0;
 }
 
 Coffee ReadOrder(uint8_t * buf, uint8_t len)
 {
-	char *order = (char*)buf;
-	if(!strcmp(order, STR_AMERICANO))
-		return AMERICANO;
-	else if(!strcmp(order, STR_CAPPUCCINO))
-		return CAPPUCCINO;
-	else if(!strcmp(order, STR_ESPRESSO))
-		return ESPRESSO;
-	else if(!strcmp(order, STR_MOCHA))
-		return MOCHA;
-	else if(!strncmp(order, STR_FLAVOUR, strlen(STR_FLAVOUR)))
-		return FLAVOUR;
+    char *order = (char *) buf;
+    if (!strcmp(order, STR_AMERICANO))
+	return AMERICANO;
+    else if (!strcmp(order, STR_CAPPUCCINO))
+	return CAPPUCCINO;
+    else if (!strcmp(order, STR_ESPRESSO))
+	return ESPRESSO;
+    else if (!strcmp(order, STR_MOCHA))
+	return MOCHA;
+    else if (!strncmp(order, STR_FLAVOUR, strlen(STR_FLAVOUR)))
+	return FLAVOUR;
 
-	return EMPTY;
+    return EMPTY;
 }
 
-int AddSugar(uint8_t * buf)
+#define MOOD 0
+int AddSugar(char *buf, int degree)
 {
-	int i, degree;
-	char *cup = (char*)buf;
+    int i;
+    char *cup = (char *) buf;
 
-	degree = mi_rand()%MAX_DEGREE;
-	for(i = 0; i < degree; i++)
-		strcat(cup, SUGAR);
+    if (degree == MOOD)
+	degree = mi_rand() % MAX_DEGREE;
+    for (i = 0; i < degree; i++)
+	strcat(cup, SUGAR);
 
-	return degree;
+    return degree;
 }
 
-int AddMilk(uint8_t * buf)
+int AddMilk(char *buf, int degree)
 {
-	int i, degree;
-	char *cup = (char*)buf;
+    int i;
+    char *cup = (char *) buf;
 
-	degree = mi_rand()%MAX_DEGREE;
-	for(i = 0; i < degree; i++)
-		strcat(cup, MILK);
+    if (degree == MOOD)
+	degree = mi_rand() % MAX_DEGREE;
+    for (i = 0; i < degree; i++)
+	strcat(cup, MILK);
 
-	return degree;
+    return degree;
 }
-int
-LeakyCoffee_S (uint8_t * inbuf, size_t inlen, uint8_t * outbuf,
-		size_t * outlen)
+
+int LeakyCoffee_S(uint8_t * inbuf, size_t inlen, uint8_t * outbuf, size_t * outlen)
 {
-	char *cup = (char*)outbuf;
+    char *cup = (char *) outbuf;
 
-	memset(outbuf, 0, *outlen);
-	switch(ReadOrder(inbuf, inlen))
-	{
-		case AMERICANO:
-			printf("%s ordered.\n", STR_AMERICANO);
-			strcat(cup, STR_AMERICANO);
-			break;
+    memset(outbuf, 0, *outlen);
+    switch (ReadOrder(inbuf, inlen))
+      {
+      case AMERICANO:
+	  printf("%s ordered.\n", STR_AMERICANO);
+	  strcat(cup, STR_AMERICANO);
+	  AddMilk(cup, MOOD);
+	  AddSugar(cup, MOOD);
+	  break;
 
-		case CAPPUCCINO:
-			printf("%s ordered.\n", STR_CAPPUCCINO);
-			strcat(cup, STR_CAPPUCCINO);
-			break;
+      case CAPPUCCINO:
+	  printf("%s ordered.\n", STR_CAPPUCCINO);
+	  strcat(cup, STR_CAPPUCCINO);
+	  AddMilk(cup, MOOD);
+	  AddSugar(cup, MOOD);
+	  break;
 
-		case ESPRESSO:
-			printf("%s ordered.\n", STR_ESPRESSO);
-			strcat(cup, STR_ESPRESSO);
-			break;
+      case ESPRESSO:
+	  printf("%s ordered.\n", STR_ESPRESSO);
+	  strcat(cup, STR_ESPRESSO);
+	  break;
 
-		case MOCHA:
-			printf("%s ordered.\n", STR_MOCHA);
-			strcat(cup, STR_MOCHA);
-			break;
+      case MOCHA:
+	  printf("%s ordered.\n", STR_MOCHA);
+	  strcat(cup, STR_MOCHA);
+	  AddMilk(cup, MOOD);
+	  AddSugar(cup, MOOD);
+	  break;
 
-		case FLAVOUR:
-			printf("More FLAVOUR required.\n");
-			strcat(cup, STR_FLAVOUR);
-			break;
+      case FLAVOUR:
+	  printf("More FLAVOUR required.\n");
+	  strcat(cup, STR_FLAVOUR);
+	  break;
 
-		default:
-			printf("Ignored bad order.\n");
-			return 0;
-	}
+      default:
+	  printf("Ignored bad order.\n");
+	  return 0;
+      }
+    *outlen = strlen((char *) outbuf);
 
-	return 1;
+    return 1;
 }
--- tinydtls-0.8.1/mi_util.h
+++ tinydtls-0.8.1/mi_util.h
@@ -8,12 +8,12 @@
 unsigned int mi_rand();
 
 //For Single_Or_Even Toy
-void SingleOrEven_C(void *buf, size_t *len);
-int SingleOrEven_S(void *inbuf, size_t inlen, void* outbuf, size_t *outlen);
+void SingleOrEven_C(void *buf, size_t * len);
+int SingleOrEven_S(void *inbuf, size_t inlen, void *outbuf, size_t * outlen);
 
 //For Leaky Coffee Toy
-int OrderCoffee_C(uint8_t *buf, size_t *len);
-int LeakyCoffee_C(uint8_t *buf, size_t *len);
-int LeakyCoffee_S(uint8_t *inbuf, size_t inlen, uint8_t* outbuf, size_t *outlen);
+int OrderCoffee_C(uint8_t * buf, size_t * len);
+int LeakyCoffee_C(uint8_t * buf, size_t * len);
+int LeakyCoffee_S(uint8_t * inbuf, size_t inlen, uint8_t * outbuf, size_t * outlen);
 
 #endif
