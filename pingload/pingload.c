892fd710a25c399a852503ac34d898e0dcd118cf
Salties <y.yan@bristol.ac.uk>
2015-07-30 16:50:27 +0100

    Update Thu Jul 30 16:50:27 BST 2015

--- pingload/pingload.c
+++ pingload/pingload.c
@@ -36,20 +36,33 @@
  * \author
  *         Adam Dunkels <adam@sics.se>
  */
-
 #include "contiki.h"
+#include "sys/etimer.h"
+#include "dev/leds.h"
 
 #include <stdio.h> /* For printf() */
 /*---------------------------------------------------------------------------*/
 PROCESS(hello_world_process, "Hello world process");
 AUTOSTART_PROCESSES(&hello_world_process);
 /*---------------------------------------------------------------------------*/
+
 PROCESS_THREAD(hello_world_process, ev, data)
 {
+  static struct etimer et;
+
   PROCESS_BEGIN();
 
   printf("Hello, world\n");
-  
+
+  leds_init();
+
+  while(1)
+  {
+	  leds_toggle(LEDS_RED);
+	  etimer_set(&et, 5 * CLOCK_SECOND);
+	  PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+  }
+   
   PROCESS_END();
 }
 /*---------------------------------------------------------------------------*/
