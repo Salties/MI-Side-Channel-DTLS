fa2bd3d60b2010525f69f8ecc17a513ad6ba2082
Salties <y.yan@bristol.ac.uk>
2015-09-14 16:22:26 +0100

    Update 2015年 09月 14日 星期一 16:22:26 BST

--- tinydtls-0.8.2/examples/contiki/obj_cc2538dk/ecc.d
+++
@@ -1,4 +0,0 @@
-obj_cc2538dk/ecc.o: ../../../../apps/tinydtls/ecc/ecc.c \
- ../../../../apps/tinydtls/ecc/ecc.h
-../../../../apps/tinydtls/ecc/ecc.c :
- ../../../../apps/tinydtls/ecc/ecc.h :
--- tinydtls-0.8.2/examples/contiki/obj_cc2538dk/rijndael.d
+++
@@ -1,4 +0,0 @@
-obj_cc2538dk/rijndael.o: ../../../../apps/tinydtls/aes/rijndael.c \
- ../../../../apps/tinydtls/aes/rijndael.h
-../../../../apps/tinydtls/aes/rijndael.c :
- ../../../../apps/tinydtls/aes/rijndael.h :
