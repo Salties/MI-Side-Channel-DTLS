ab8f121da8046c26c2469aee4444526b8dca4ca7
Salties <y.yan@bristol.ac.uk>
2015-09-14 11:03:07 +0100

    Update 2015年 09月 14日 星期一 11:03:07 BST

--- tinydtls-0.8.2/examples/contiki/obj_wismote/rijndael.d
+++
@@ -1,4 +0,0 @@
-obj_wismote/rijndael.o: ../../../../apps/tinydtls/aes/rijndael.c \
- ../../../../apps/tinydtls/aes/rijndael.h
-../../../../apps/tinydtls/aes/rijndael.c :
- ../../../../apps/tinydtls/aes/rijndael.h :
