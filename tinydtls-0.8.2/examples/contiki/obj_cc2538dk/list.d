7a5d92adec41de7fc209a047cf1eca736f67b7cf
Salties <y.yan@bristol.ac.uk>
2015-09-14 16:22:26 +0100

    Update 2015年 09月 14日 星期一 16:22:26 BST

--- tinydtls-0.8.2/examples/contiki/obj_cc2538dk/list.d
+++
@@ -1,4 +0,0 @@
-obj_cc2538dk/list.o: ../../../../core/lib/list.c \
- ../../../../core/./lib/list.h
-../../../../core/lib/list.c :
- ../../../../core/./lib/list.h :
--- tinydtls-0.8.2/examples/contiki/obj_wismote/ecc.d
+++
@@ -1,4 +0,0 @@
-obj_wismote/ecc.o: ../../../../apps/tinydtls/ecc/ecc.c \
- ../../../../apps/tinydtls/ecc/ecc.h
-../../../../apps/tinydtls/ecc/ecc.c :
- ../../../../apps/tinydtls/ecc/ecc.h :
