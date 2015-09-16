423fff8945c4bad59321a603a1d8ae6d2c32567e
Salties <y.yan@bristol.ac.uk>
2015-09-16 11:08:17 +0100

    Update 2015年 09月 16日 星期三 11:08:17 BST

--- tinydtls-0.8.2/examples/contiki/obj_cc2538dk/dbg-sprintf.d
+++
@@ -1,5 +0,0 @@
-obj_cc2538dk/dbg-sprintf.o: \
- /home/yy12135/MyRepository/contiki/cpu/cc2538/../arm/common/dbg-io/dbg-sprintf.c \
- /home/yy12135/MyRepository/contiki/cpu/cc2538/../arm/common/dbg-io/strformat.h
- /home/yy12135/MyRepository/contiki/cpu/cc2538/../arm/common/dbg-io/dbg-sprintf.c :
- /home/yy12135/MyRepository/contiki/cpu/cc2538/../arm/common/dbg-io/strformat.h :
