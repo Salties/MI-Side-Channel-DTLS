ce3a8fac7689d917afd5949d6f1b4f1695dede62
Salties <y.yan@bristol.ac.uk>
2015-09-17 10:35:37 +0100

    Update 2015年 09月 17日 星期四 10:35:37 BST

--- tinydtls-0.8.2/examples/contiki/obj_wismote/list.d
+++
@@ -1,4 +0,0 @@
-obj_wismote/list.o: /home/yy12135/MyRepository/contiki/core/lib/list.c \
- /home/yy12135/MyRepository/contiki/core/./lib/list.h
-/home/yy12135/MyRepository/contiki/core/lib/list.c :
- /home/yy12135/MyRepository/contiki/core/./lib/list.h :
