263d175d63d6222a5123fbb44fa773934c1d2b51
Salties <y.yan@bristol.ac.uk>
2015-09-17 17:41:18 +0100

    Update 2015年 09月 17日 星期四 17:41:18 BST

--- dtlsserver/obj_cc2538dk/rijndael.d
+++
@@ -1,4 +0,0 @@
-obj_cc2538dk/rijndael.o: ../contiki/apps/tinydtls/aes/rijndael.c \
- ../contiki/apps/tinydtls/aes/rijndael.h
-../contiki/apps/tinydtls/aes/rijndael.c :
- ../contiki/apps/tinydtls/aes/rijndael.h :
