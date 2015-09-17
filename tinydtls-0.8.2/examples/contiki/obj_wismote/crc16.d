715a894305ef0f2864b90eeb4da0a8a41be2563d
Salties <y.yan@bristol.ac.uk>
2015-09-17 10:35:37 +0100

    Update 2015年 09月 17日 星期四 10:35:37 BST

--- tinydtls-0.8.2/examples/contiki/obj_wismote/crc16.d
+++
@@ -1,2 +0,0 @@
-obj_wismote/crc16.o: /home/yy12135/MyRepository/contiki/core/lib/crc16.c
-/home/yy12135/MyRepository/contiki/core/lib/crc16.c :
--- tinydtls-0.8.2/examples/contiki/obj_wismote/rijndael.d
+++
@@ -1,5 +0,0 @@
-obj_wismote/rijndael.o: \
- /home/yy12135/MyRepository/contiki/apps/tinydtls/aes/rijndael.c \
- /home/yy12135/MyRepository/contiki/apps/tinydtls/aes/rijndael.h
- /home/yy12135/MyRepository/contiki/apps/tinydtls/aes/rijndael.c :
- /home/yy12135/MyRepository/contiki/apps/tinydtls/aes/rijndael.h :
