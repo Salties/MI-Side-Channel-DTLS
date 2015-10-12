84ccbbf5a2820930ffad45ef4a4a43e92add414e
Salties <y.yan@bristol.ac.uk>
2015-10-12 11:02:16 +0100

    Update 2015年 10月 12日 星期一 11:02:16 BST

--- mydtls/obj_cc2538dk/me.d
+++
@@ -1,4 +0,0 @@
-obj_cc2538dk/me.o: /home/yy12135/MyRepository/contiki/core/lib/me.c \
- /home/yy12135/MyRepository/contiki/core/lib/me_tabs.h
-/home/yy12135/MyRepository/contiki/core/lib/me.c :
- /home/yy12135/MyRepository/contiki/core/lib/me_tabs.h :
