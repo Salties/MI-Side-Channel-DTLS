82b38e9b71666b815c2647c7b13cb315b2d30505
Salties <y.yan@bristol.ac.uk>
2015-10-12 11:13:08 +0100

    Update 2015年 10月 12日 星期一 11:13:08 BST

---
+++ llsec_experiment/project-conf.h
@@ -0,0 +1,31 @@
+#ifndef __PROJECT_CONF_H__
+#define __PROJECT_CONF_H__
+
+#define UIP_CONF_TCP                   0
+
+//RDC driver
+//#undef NETSTACK_CONF_RDC
+//#define NETSTACK_CONF_RDC nullrdc_driver
+//#define NETSTACK_CONF_RDC contikimac_driver
+//#define NETSTACK_CONF_RDC sicslowmac_driver
+//#define NETSTACK_CONF_RDC cxmac_driver
+
+//MAC driver
+//#undef NETSTACK_CONF_MAC
+//#define NETSTACK_CONF_MAC nullmac_driver
+
+//#define NETSTACK_CONF_RDC_CHANNEL_CHECK_RATE 8
+
+#undef NETSTACK_CONF_LLSEC
+#define NETSTACK_CONF_LLSEC               noncoresec_driver
+#undef IEEE802154_CONF_SECURITY_LEVEL
+#define IEEE802154_CONF_SECURITY_LEVEL   2
+
+#define NONCORESEC_CONF_KEY {\
+	0x00 , 0x01 , 0x02 , 0x03 ,\
+	0x04 , 0x05 , 0x06 , 0x07 ,\
+	0x08 , 0x09 , 0x0A , 0x0B ,\
+	0x0C , 0x0D , 0x0E , 0x0F \
+}
+
+#endif
