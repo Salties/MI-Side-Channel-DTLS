d8822c891b2bea4d7b4f655e09036f28e34fb8e3
Salties <y.yan@bristol.ac.uk>
2015-10-15 13:52:24 +0100

    Update 2015年 10月 15日 星期四 13:52:24 BST

--- llsec_experiment/broadcast-example.c
+++ llsec_experiment/broadcast-example.c
@@ -50,47 +50,50 @@
 
 static struct simple_udp_connection broadcast_connection;
 
-/*---------------------------------------------------------------------------*/
-PROCESS (broadcast_example_process, "UDP broadcast example process");
-AUTOSTART_PROCESSES (&broadcast_example_process);
-/*---------------------------------------------------------------------------*/
-static void
-receiver (struct simple_udp_connection *c,
-	  const uip_ipaddr_t * sender_addr,
-	  uint16_t sender_port,
-	  const uip_ipaddr_t * receiver_addr,
-	  uint16_t receiver_port, const uint8_t * data, uint16_t datalen)
-{
-  printf ("Data received on port %d from port %d with length %d\n",
-	  receiver_port, sender_port, datalen);
-}
+static uint8_t sendbuf[4]={0x00,0x00,0x00,0x00};
+//static uint8_t sendbuf[4]={0xff,0xff,0xff,0xff};
 
 /*---------------------------------------------------------------------------*/
-PROCESS_THREAD (broadcast_example_process, ev, data)
+PROCESS(broadcast_example_process, "UDP broadcast example process");
+AUTOSTART_PROCESSES(&broadcast_example_process);
+/*---------------------------------------------------------------------------*/
+static void
+receiver(struct simple_udp_connection *c,
+         const uip_ipaddr_t *sender_addr,
+         uint16_t sender_port,
+         const uip_ipaddr_t *receiver_addr,
+         uint16_t receiver_port,
+         const uint8_t *data,
+         uint16_t datalen)
+{
+  printf("Data received on port %d from port %d with length %d\n",
+         receiver_port, sender_port, datalen);
+}
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(broadcast_example_process, ev, data)
 {
   static struct etimer periodic_timer;
   static struct etimer send_timer;
   uip_ipaddr_t addr;
 
-  PROCESS_BEGIN ();
+  PROCESS_BEGIN();
 
-  simple_udp_register (&broadcast_connection, UDP_PORT,
-		       NULL, UDP_PORT, receiver);
+  simple_udp_register(&broadcast_connection, UDP_PORT,
+                      NULL, UDP_PORT,
+                      receiver);
 
-  etimer_set (&periodic_timer, SEND_INTERVAL);
-  while (1)
-    {
-      PROCESS_WAIT_EVENT_UNTIL (etimer_expired (&periodic_timer));
-      etimer_reset (&periodic_timer);
-      etimer_set (&send_timer, SEND_TIME);
+  etimer_set(&periodic_timer, SEND_INTERVAL);
+  while(1) {
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
+    etimer_reset(&periodic_timer);
+    etimer_set(&send_timer, SEND_TIME);
 
-      PROCESS_WAIT_EVENT_UNTIL (etimer_expired (&send_timer));
-      printf ("Sending broadcast\n");
-      uip_create_linklocal_allnodes_mcast (&addr);
-      simple_udp_sendto (&broadcast_connection, "Test", 4, &addr);
-    }
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&send_timer));
+    printf("Sending broadcast\n");
+    uip_create_linklocal_allnodes_mcast(&addr);
+    simple_udp_sendto(&broadcast_connection, sendbuf, 4, &addr);
+  }
 
-  PROCESS_END ();
+  PROCESS_END();
 }
-
 /*---------------------------------------------------------------------------*/
