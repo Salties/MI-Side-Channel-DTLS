542b1c4258a38f99241ed552be2c3d18d06dc5d1
Salties <y.yan@bristol.ac.uk>
2016-03-02 15:19:48 +0000

    Add key stroke request to send in dtls-client.c

--- tinydtls-0.8.2/tests/dtls-client.c
+++ tinydtls-0.8.2/tests/dtls-client.c
@@ -196,7 +196,7 @@ send_to_peer(struct dtls_context_t *ctx,
 	     session_t *session, uint8 *data, size_t len) {
 
   int fd = *(int *)dtls_get_app_data(ctx);
-  //Wait for a key stroke
+  printf("Press Enter to send a packet:");
   getchar();
   return sendto(fd, data, len, MSG_DONTWAIT,
 		&session->addr.sa, session->size);
