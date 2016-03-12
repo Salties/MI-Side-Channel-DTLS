4985903f13dcebe565010b9f07e1d863f0667f5e
Salties <y.yan@bristol.ac.uk>
2016-03-12 21:08:36 +0000

    Cleam tinydtls doc

--- tinydtls-0.8.2/doc/html/search/files_1.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['ccm_2eh',['ccm.h',['../ccm_8h.html',1,'']]],
-  ['crypto_2eh',['crypto.h',['../crypto_8h.html',1,'']]]
-];
--- tinydtls-0.8.2/doc/html/search/variables_11.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['verify_5fecdsa_5fkey',['verify_ecdsa_key',['../structdtls__handler__t.html#a80c44baf0a59be0fd842358e651deaf4',1,'dtls_handler_t']]]
-];
