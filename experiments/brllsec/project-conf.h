44f94e4d5a1745f64951a76571b767008a81a8c2
Salties <y.yan@bristol.ac.uk>
2016-03-13 13:24:49 +0000

    Added for brllsec

--- experiments/brllsec/project-conf.h
+++ experiments/brllsec/project-conf.h
@@ -53,4 +53,17 @@
 #endif
 #endif
 
+#undef NETSTACK_CONF_LLSEC
+#define NETSTACK_CONF_LLSEC noncoresec_driver
+#undef LLSEC802154_CONF_SECURITY_LEVEL
+#define LLSEC802154_CONF_SECURITY_LEVEL 7
+
+#define NONCORESEC_CONF_KEY {\
+	0x00 , 0x01 , 0x02 , 0x03 ,\
+	0x04 , 0x05 , 0x06 , 0x07 ,\
+	0x08 , 0x09 , 0x0A , 0x0B ,\
+	0x0C , 0x0D , 0x0E , 0x0F \
+}
+
+
 #endif /* PROJECT_ROUTER_CONF_H_ */
