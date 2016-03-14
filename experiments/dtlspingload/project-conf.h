5b64a9e1962e22de3342aa452a9a9d5c31092ea9
Salties <y.yan@bristol.ac.uk>
2016-03-14 17:59:57 +0000

    Why did I changed the RDC driver??????????

--- experiments/dtlspingload/project-conf.h
+++ experiments/dtlspingload/project-conf.h
@@ -4,9 +4,9 @@
 #define UIP_CONF_TCP                   0
 
 //RDC driver
-#undef NETSTACK_CONF_RDC
+//#undef NETSTACK_CONF_RDC
 //#define NETSTACK_CONF_RDC nullrdc_driver
-#define NETSTACK_CONF_RDC contikimac_driver
+//#define NETSTACK_CONF_RDC contikimac_driver
 //#define NETSTACK_CONF_RDC sicslowmac_driver
 //#define NETSTACK_CONF_RDC cxmac_driver
 
