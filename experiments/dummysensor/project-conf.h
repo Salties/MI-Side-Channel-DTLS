52506efe4efc9a682ce2ddbfa6e53460f965619e
Salties <y.yan@bristol.ac.uk>
2016-03-17 12:14:19 +0000

    Added relaxed border router.

--- experiments/dummysensor/project-conf.h
+++ experiments/dummysensor/project-conf.h
@@ -3,11 +3,11 @@
 
 #define UIP_CONF_TCP                   0
 
-#ifdef NULL_DRIVER
-#define NETSTACK_CONF_RDC nullrdc_driver
-#define NETSTACK_CONF_MAC nullmac_driver
-#endif
+//#ifdef NULL_DRIVER
+//#define NETSTACK_CONF_RDC nullrdc_driver
+//#define NETSTACK_CONF_MAC nullmac_driver
+//#endif
 
-#define NETSTACK_CONF_RDC_CHANNEL_CHECK_RATE 8
+//#define NETSTACK_CONF_RDC_CHANNEL_CHECK_RATE 8
 
 #endif
