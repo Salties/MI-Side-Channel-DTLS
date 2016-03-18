c7399fa4603f1dbd4389b9c953d48769b5301e14
Salties <y.yan@bristol.ac.uk>
2016-03-18 20:18:50 +0000

    Added PINGLOAD data. LLSEC update.

--- experiments/keyllsec/project-conf.h
+++ experiments/keyllsec/project-conf.h
@@ -6,8 +6,6 @@
 #define UIP_CONF_TCP 0
 #endif
 
-#define ENABLE_LLSEC 1
-
 //RDC driver
 //#undef NETSTACK_CONF_RDC
 //#define NETSTACK_CONF_RDC nullrdc_driver
@@ -21,7 +19,8 @@
 
 //#define NETSTACK_CONF_RDC_CHANNEL_CHECK_RATE 8
 
-#if (ENABLE_LLSEC == 1)
+#ifdef ENABLE_LLSEC
+
 #undef NETSTACK_CONF_LLSEC
 #define NETSTACK_CONF_LLSEC noncoresec_driver
 #undef LLSEC802154_CONF_SECURITY_LEVEL
@@ -32,7 +31,7 @@
 	0x04 , 0x05 , 0x06 , 0x07 ,\
 	0x08 , 0x09 , 0x0A , 0x0B ,\
 	0x0C , 0x0D , 0x0E , 0x0F \
-}
-#endif
+} 
+#endif //End of #ifdef ENABLE_LLSEC
 
 #endif
