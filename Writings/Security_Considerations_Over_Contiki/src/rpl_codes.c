0b3b5d666c7fb0dcb533e03cfc9b2ec198ad88c0
Salties <y.yan@bristol.ac.uk>
2016-03-20 16:34:00 +0000

    Update LLSEC.

---
+++ Writings/Security_Considerations_Over_Contiki/src/rpl_codes.c
@@ -0,0 +1,9 @@
+/* RPL message types */
+#define RPL_CODE_DIS                   0x00   /* DAG Information Solicitation */
+#define RPL_CODE_DIO                   0x01   /* DAG Information Option */
+#define RPL_CODE_DAO                   0x02   /* Destination Advertisement Option */
+#define RPL_CODE_DAO_ACK               0x03   /* DAO acknowledgment */
+#define RPL_CODE_SEC_DIS               0x80   /* Secure DIS */
+#define RPL_CODE_SEC_DIO               0x81   /* Secure DIO */
+#define RPL_CODE_SEC_DAO               0x82   /* Secure DAO */
+#define RPL_CODE_SEC_DAO_ACK           0x83   /* Secure DAO ACK */
