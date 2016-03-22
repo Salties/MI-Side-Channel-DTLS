00a6a14813e80ee503d8bbbb1e3caeece6c85c13
Salties <y.yan@bristol.ac.uk>
2016-03-22 18:26:29 +0000

    LLSEC update.

--- Writings/Security_Considerations_Over_Contiki/src/aes_if.c
+++ Writings/Security_Considerations_Over_Contiki/src/aes_if.c
@@ -1,4 +1,6 @@
 #include "lib/aes-128.h"
 
-void AES_128.set_key(AesKey); //Set the key to AesKey.
-void AES_128.encrypt(Data);   //Data is used for both plaintext and ciphertext.
+//Set the key to AesKey.
+void AES_128.set_key(AesKey);
+//Data is used for both plaintext and ciphertext.
+void AES_128.encrypt(Data);   
