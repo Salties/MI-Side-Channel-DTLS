a5ecbc292e33ef920cb0042d2d7fad31ac0cf390
Salties <y.yan@bristol.ac.uk>
2016-03-25 20:23:23 +0000

    LLSEC proof reading done.

--- Writings/Security_Considerations_Over_Contiki/src/aes_timing.c
+++ Writings/Security_Considerations_Over_Contiki/src/aes_timing.c
@@ -4,7 +4,7 @@ AES_128.set_key(Aes128Key);
 start = RTIMER_NOW();
 for (i = 0; i < n; i++) 
 { 
-    AES_128.encrypt(datablock[i]);
+    AES_128.encrypt(datablock[i]); //Used for both plaintext and ciphertext.
 }
 end = RTIMER_NOW();
 AesTime = end - start;
