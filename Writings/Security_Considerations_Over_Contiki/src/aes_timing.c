f53a2146be37cd2d521521c9eadd9abfeb9495c3
Salties <y.yan@bristol.ac.uk>
2016-04-01 12:23:15 +0100

    Update llsec.

--- Writings/Security_Considerations_Over_Contiki/src/aes_timing.c
+++ Writings/Security_Considerations_Over_Contiki/src/aes_timing.c
@@ -1,5 +1,5 @@
 uint8_t datablock[n][AES_BLOCK_LEN];
-/* ... */
+/*...Initialisation... */
 AES_128.set_key(Aes128Key);
 start = RTIMER_NOW();
 for (i = 0; i < n; i++) 
@@ -7,4 +7,5 @@ for (i = 0; i < n; i++)
     AES_128.encrypt(datablock[i]); //Used for both plaintext and ciphertext.
 }
 end = RTIMER_NOW();
-AesTime = end - start;
+AesExecutionTime = end - start;
+/*...Finalisation...*/
