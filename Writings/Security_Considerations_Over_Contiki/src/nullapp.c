d3455f9588dae3ece162f94777fa4299c6105212
Salties <y.yan@bristol.ac.uk>
2016-04-03 21:39:01 +0100

    DTLS timing update. Data update.

---
+++ Writings/Security_Considerations_Over_Contiki/src/nullapp.c
@@ -0,0 +1,9 @@
+static const uint8_t ConstantValue[MAX_PAYLOAD_LEN] = {{0}};
+
+static int ApplicationCode(/*...DTLS arguments...*/) 
+{  
+  //Replies with a constant value on Requests.
+  dtls_write(ctx, session, ConstantValue, RESPONSE_LENGTH);
+  
+  return 0;
+}
