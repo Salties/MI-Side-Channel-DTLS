19e7951f8c01190e38ff3b1f711500a0f18325cc
Salties <y.yan@bristol.ac.uk>
2016-04-05 20:35:21 +0100

    DTLS application code timing data update.

---
+++ Writings/Security_Considerations_Over_Contiki/src/dtlsapptime.c
@@ -0,0 +1,13 @@
+static int ApplicationCode(/*...DTLS arguments...*/) 
+{
+  clock_tick_t resp;
+  //Time the execution of random_rand().
+  start = RTIMER_NOW();
+  for(i=0; i < RequestData; i++)
+	  random_rand();
+  end = RTIMER_NOW();  
+  //Reply with the execution time.
+  resp = end - start;
+  dtls_write(ctx, session, &resp, sizeof(resp));
+  return 0;
+}
