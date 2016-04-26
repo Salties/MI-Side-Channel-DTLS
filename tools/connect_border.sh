45b32e0521ab1e394079972a56414c6087702098
Salties <y.yan@bristol.ac.uk>
2016-04-26 18:44:57 +0100

    Now supports optional device.

--- tools/connect_border.sh
+++ tools/connect_border.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-if [[ $1 = "stop" ]]; then
-	sudo killall tunslip6;
-	echo "stop.";
-	exit;
+device="/dev/ttyUSB0"
+
+if [[ ! -z $1 ]]; then
+	device=$1;
 fi
+
+echo $1
 
 TUNSLIP=${CONTIKI_ROOT}/tools/tunslip6
 
-sudo $TUNSLIP -s /dev/ttyUSB0 aaaa::1/64
+sudo $TUNSLIP -s ${device} aaaa::1/64
 
 if [[ $? = "0" ]];
 then
