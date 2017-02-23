d1f2e0c8a19bf3a39c9db33d80297c74b450983a
Saltiny <saltiny@ubuntu.ubuntu-domain>
2017-02-23 12:14:38 +0000

    Added setup script.

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+CONTIKI_ROOT=`pwd`/contiki/
+CONTIKI_TOOLS=`pwd`/tools/
+
+if [[ `grep 'CONTIKI_ROOT' -c ~/.bashrc` == '0' ]]; then
+	echo "Adding CONTIKI_ROOT to bashrc..."
+	echo "export CONTIKI_ROOT=${CONTIKI_ROOT}" #>> ~/.bashrc;
+else
+	echo "CONTIKI_ROOT already exists."
+fi;
+
+if [[ `grep 'CONTIKI_TOOLS' -c ~/.bashrc` == '0' ]]; then
+	echo "Adding CONTIKI_TOOLS to bashrc..."
+	echo "export CONTIKI_TOOLS=${CONTIKI_TOOLS}" >> ~/.bashrc;
+	echo "PATH+=:${CONTIKI_TOOLS}" >> ~/.bashrc
+else
+	echo "CONTIKI_TOOLS already exists."
+fi;
