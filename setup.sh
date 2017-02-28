9d6166d7ff158c7ce155a252812f6fd3d1fc8bab
Salties <salties@ubuntu.ubuntu-domain>
2017-02-28 16:48:23 +0000

    Added my editorial configuration.

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ CONTIKI_TOOLS=`pwd`/tools/
 
 if [[ `grep 'CONTIKI_ROOT' -c ~/.bashrc` == '0' ]]; then
 	echo "Adding CONTIKI_ROOT to bashrc..."
-	echo "export CONTIKI_ROOT=${CONTIKI_ROOT}" #>> ~/.bashrc;
+	echo "export CONTIKI_ROOT=${CONTIKI_ROOT}" >> ~/.bashrc;
 else
 	echo "CONTIKI_ROOT already exists."
 fi;
@@ -17,3 +17,5 @@ if [[ `grep 'CONTIKI_TOOLS' -c ~/.bashrc` == '0' ]]; then
 else
 	echo "CONTIKI_TOOLS already exists."
 fi;
+
+echo "Setup complete. Please restart the terminal."
