bdef938ef861a2d46b2094c2de0c6e9ce1402e98
salties <y.yan@bristol.ac.uk>
2018-01-17 17:38:45 +0000

    NEW: Added '-r'.

--- tools/trsdump.py
+++ tools/trsdump.py
@@ -10,11 +10,14 @@ compressed = False
 start = 0
 end = float('inf')
 
+PrintHeaderInfo = False
+
 
 def PrintHelp():
     print('Dump trs files as TraceSet object.'),
     print('Usage: trsdump.py [-c] [-s START] [-e END] TRSFILE DUMPFILE')
-    print('\t-t : Print this message.')
+    print('\t-h : Print this message.')
+    print('\t-r : Print TRS header info in TRSFILE.')
     print('\t-c : Use gzip compression.')
     print('\t-s : Use points from START in each trace.')
     print('\t-e : Use points from END in each trace.')
@@ -23,7 +26,7 @@ def PrintHelp():
 
 # Parse command line arguments.
 def ParseArgs(argc, argv):
-    global iFile, oFile, compressed, start, end
+    global iFile, oFile, compressed, start, end, PrintHeaderInfo
 
     if argc < 3:
         return False
@@ -33,6 +36,9 @@ def ParseArgs(argc, argv):
         if argv[i] == '-h':  # Print help message.
             PrintHelp()
             exit(0)
+
+        elif argv[i] == '-i':  # Print TRS headers only.
+            PrintHeaderInfo = True
 
         elif argv[i] == '-c':  # Use gzip compress.
             compressed = True
@@ -65,20 +71,27 @@ def main(argc, argv):
         PrintHelp()
         return -1
 
+    if PrintHeaderInfo:
+        trsheaders = traces.ReadTrsHeader(open(iFile, 'rb'))
+        for (k, v) in trsheaders.items():
+            print(k, "\t: ", v)
+        return
+
     if compressed:
         fd = gzip.open(oFile, 'wb')
     else:
-        print('#')
         fd = open(oFile, 'wb')
 
     try:
         ts = traces.TraceSet(
             iFile, start, end, showprogress=True, showheader=True)
+
         ts.Dump(fd)
+
+        print('Dump trs as TraceSet object (ver: {})'.format(ts.version))
+
     finally:
         fd.close()
-
-    print('Dump trs as TraceSet object (ver: {})'.format(ts.version))
 
     return
 
