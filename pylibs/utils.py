0f36e7f0858d87183838a0ddb0c755f07a3907cb
Salties <salties@ubuntu.ubuntu-domain>
2018-01-30 14:25:04 +0000

    Comment and style fix.

--- pylibs/utils.py
+++ pylibs/utils.py
@@ -2,19 +2,21 @@
 
 import gmpy
 import math
-import scipy.stats 
+import scipy.stats
 
 
+# Compute the Hamming Weight of x.
 def HW(x):
     return gmpy.popcount(x)
 
 
-# Right rotate i by n bits for N-bits register.
+# Right rotate x by i bits for N-bits register.
 def rotr(x, i, N):
     i = i % N
     return (2**N - 1) & ((x << (N - i)) | (x >> i))
 
 
+# Computes the Shannon's Entropy for the list L.
 def ShannonEntropy(L):
     # Compute the empirical probability for each element in L
     X = dict()
@@ -25,10 +27,10 @@ def ShannonEntropy(L):
             X[i] += 1
 
     N = len(L)
-    P = [float(X[i]/len(L)) for i in X]
+    P = [float(X[i] / len(L)) for i in X]
 
-    H = -sum([i*math.log(i,2) for i in P])
+    H = -sum([i * math.log(i, 2) for i in P])
 
-    #H = scipy.stats.entropy(P)
+    # H = scipy.stats.entropy(P)
 
     return H
